b6c86c95b88e8cdf2d1c002197b16026b76a4b2e
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2024-02-08 20:38:25 -0300

    feat: tipos de valor

---
+++ Umfg.ProgramacaoIV.Aula08022024/Umfg.ProgramacaoIV.Aula08022024/Program.cs
@@ -0,0 +1,23 @@
+﻿using Umfg.ProgramacaoIV.Aula08022024.TiposDeValor;
+
+namespace Umfg.ProgramacaoIV.Aula08022024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TiposInteiros tiposInteiros = new TiposInteiros();
+            //var tiposInteiros = new TiposInteiros();
+
+            //tiposInteiros.Executar();
+
+            //new TiposPontoFlutuante().Executar();
+
+            //new TipoBoolean().Executar();
+
+            new TipoDateTime().Executar();
+
+            Console.ReadLine();
+        }
+    }
+}
